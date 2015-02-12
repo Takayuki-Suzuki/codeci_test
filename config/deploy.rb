e0e4c80a1fa24d1ad53a9a5ac134544f7ddd342d
鈴木貴之 <takayuki.suzuki.dr@gmail.com>
2015-02-12 15:31:50 +1100

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
 lock '3.3.5'
 
-set :application, 'codeci_test'
-set :repo_url, 'https://github.com/a-Suzuki/.git'
+set :application, 'my_app_name'
+set :repo_url, 'git@example.com:me/my_repo.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
